246a37052dc8bafd85ab79dfa45a56f17f9bc4c0
Alison Mukoma __sonlinux__ <alison.mukoma@trivago.com>
2020-11-14 21:46:05 +0100

    add example signture

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@
 # add another import like one below to import everything from your module for reuse in other places.
 # for example `from alison_bookmark import *`
 from .users import *
+from .alison_bmk import *
---
+++ tools/alison_bmk_utils.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+__author__ = "Alison Mukoma"
+__copyright__  = "DevsBranch"
+__date__ = "14/11/2020"
